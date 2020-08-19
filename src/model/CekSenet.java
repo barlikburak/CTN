933714174fa094a7f3db8375c2af24721e2fb141
barlikburak <barlikburak@hotmail.com>
2020-08-19 15:03:39 +0300

    kendi çekim kategorisi yapıldı

--- src/model/CekSenet.java
+++ src/model/CekSenet.java
@@ -2,6 +2,7 @@ package model;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  *
@@ -23,7 +24,7 @@ public class CekSenet extends AbstractMenu {
     @Override
     public void altKategoriEventHandler() {
         getvBox().getChildren().get(0).addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
-
+            start(new Stage(), "FXMLKendiCekim");
         });
         getvBox().getChildren().get(1).addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
             

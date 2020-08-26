f79db8f1381cd5566bdc44a895eb8b2e5273df37
barlikburak <barlikburak@hotmail.com>
2020-08-26 14:02:51 +0300

    kasa kategorisinin alt kategorilerinin hepsi yapıldı

--- src/model/Kasa.java
+++ src/model/Kasa.java
@@ -1,7 +1,7 @@
 package model;
 
-import javafx.fxml.FXMLLoader;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  *
@@ -11,43 +11,35 @@ public class Kasa extends AbstractMenu {
 
     public Kasa(Menu menu) {
         super(menu,"kasa");
-        altkategori = new String[8];
+        altkategori = new String[6];
         altkategori[0] = "gunluk-kasa.png";
         altkategori[1] = "ayrintili-kasa.png";
         altkategori[2] = "gider-masraf.png";
         altkategori[3] = "gelirler.png";
-        altkategori[4] = "subeler-arasi-para-transferi.png";
+        altkategori[4] = "kar-zarar-analiz.png";
         altkategori[5] = "gelir-gider-rapor.png";
-        altkategori[6] = "kar-zarar-analiz.png";
-        altkategori[7] = "mizan-raporu.png";
         eventHandler(menu);
     }
 
     @Override
     public void altKategoriEventHandler() {
         getvBox().getChildren().get(0).addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
-
+            start(new Stage(), "FXMLGunlukKasa");
         });
         getvBox().getChildren().get(1).addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
-            
+            start(new Stage(), "FXMLAyrintiliKasa");
         });
         getvBox().getChildren().get(2).addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
-            
+            start(new Stage(), "FXMLGiderMasraf");
         });
         getvBox().getChildren().get(3).addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
-            
+            start(new Stage(), "FXMLGelirler");
         });
         getvBox().getChildren().get(4).addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
-            
+            start(new Stage(), "FXMLKarZararAnaliz");
         });
         getvBox().getChildren().get(5).addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
-            
-        });
-        getvBox().getChildren().get(6).addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
-            
-        });
-        getvBox().getChildren().get(7).addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
-            
+            start(new Stage(), "FXMLGelirGiderRapor");
         });
     }
 
